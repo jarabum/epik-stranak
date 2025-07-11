1964ac53825ce3ec46cba2d3054f31b8f3fecc03
jarabum <jara11.str@gmail.com>
2025-07-11 19:07:07 +0200

    pridanej cudlik do zajimavosti.html + napsano 10 zajimavosti do zajimavosti.js

--- scripty/zajimavosti.js
+++ scripty/zajimavosti.js
@@ -1,1 +1,15 @@
 console.log("jdeme informovat lidi");
+
+const zajimavosti = [
+    "den na venusi je delsi nez jeji rok",
+    "japonci maji slovo 'tsundoku' co znamena si kupovat knihy co nikdy nectes",
+    "kleopatra zila bliz k vynalezu iphonu nez k pyramidam",
+    "mravenci preziji v mikrovlnce protoze jsou moc maly aby absorbovaly mikrovlny",
+    "kocky nemohou chutnat sladkou chut",
+    "slunce tvori 99.86% hmotnosti cele slunecni soustavy",
+    "saturn by plaval ve vode protoze ma nizsi hustotu nez voda",
+    "pocitacova mys se puvodne jmenovala 'indikátor polohy X-Y pro zobrazovací systém'",
+    "google se prvne jmenoval 'backrub'",
+    "v mongolsku je vice koni nez lidi",
+    "vcely dokazou poznat oblicej cloveka"
+]
