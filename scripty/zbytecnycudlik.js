562cdef33f86db56bb5c0be56521904d763da6b0
jarabum <jara11.str@gmail.com>
2025-10-15 18:25:29 +0200

    kdyz ted kliknes tak ti to napise do konzole pocet kliku

--- scripty/zbytecnycudlik.js
+++ scripty/zbytecnycudlik.js
@@ -1,3 +1,10 @@
 textik = document.getElementById("textik")
+console.log("hlavne se uklidnime jo")
 
 textik.textContent = "hlavne neklikej nic to neudela";
+let kliky = 0;
+
+function kliknuti(){
+    kliky += 1;
+    console.log(kliky)
+};
